b0a454835fe73bb16f8c5939f73f87d0c804e2ac
Rania Saadi <88983972+raniasaadi16@users.noreply.github.com>
2021-10-12 22:31:52 +0200

    disable redux dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,9 @@ import allReducers from './reducers';
 import thunk from 'redux-thunk'
 
 const middleware = [thunk]
-//const store = createStore(allReducers, applyMiddleware(...middleware))
-const store = createStore(allReducers, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(allReducers, applyMiddleware(...middleware))
+// const store = createStore(allReducers, compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
